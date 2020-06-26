d7df84da3074e900fa40b1901786e18ad62d69fd
SanyaNagel <student17let@mail.ru>
2020-06-26 22:17:46 +0700

    Паттерн абстрактная фабрика

---
+++ src/abstractFactory/Computer.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class Computer {
+    public Computer(){
+
+    }
+}
---
+++ src/abstractFactory/startPattern.java
@@ -0,0 +1,7 @@
+package abstractFactory;
+
+public class startPattern {
+    public startPattern(){
+
+    }
+}
